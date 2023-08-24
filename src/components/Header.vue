933cb548fe8f8b7ebe21a25c09f62153d711c40d
gabriel <musgabriel2@gmail.com>
2023-08-25 00:29:44 +0200

    nav bar style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,20 +2,36 @@
 import {RouterLink} from "vue-router"
 </script>
 <template>
-    <div>
+    <main>
         <nav class="navbar">
             <div class="Logo">
                 <h1>MY Store</h1>
             </div>
             <div class="menu">
-                <RouterLink to="">Product</RouterLink>
-                <RouterLink to="">About</RouterLink>
-                <RouterLink to="">Contact</RouterLink>
-                <RouterLink to="">Card</RouterLink>
-                <p>hi</p>
+                <a href="">Product</a>
+                <a href="">About</a>
+                <a href="">Contact</a>
+            
                 
             </div>
         </nav>
         
-    </div>
+    </main>
 </template>
+
+<style scoped>
+main{
+    margin:0;
+    padding: 0;
+    font-family: sans-serif;
+}
+.navbar {
+    padding: 20px 5%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    
+
+}
+
+</style>
