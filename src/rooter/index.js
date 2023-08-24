ed9dd3238ba7f11e0630d0a69b27707cd24afe2b
gabriel <musgabriel2@gmail.com>
2023-08-25 01:58:09 +0200

    routing rules setup

--- src/rooter/index.js
+++ src/rooter/index.js
@@ -1,8 +1,36 @@
-// Define Routing Rules
+//  Define our Routing Rules
 
 import { createRouter,createWebHistory } from "vue-router";
-import ProductView from "../views/ProductView.vue"
+import ProductView from '../views/ProductView.vue';
+
+
+
 
 const router = createRouter({
-    
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes:[
+        // {
+        //     path:"/",
+        //     name:"home",
+        //     component:HomeView
+        // },
+        {
+            path:"/about",
+            name:"about",
+            component:AboutView
+        }
+        // {
+        //     path:"/cars/:id",
+        //     name:"car",
+        //     component:CarView,
+        //     children: [
+        //         {
+        //             path:"contact",
+        //             component:ContactView
+        //         }
+        //     ]
+        // }
+    ]
 })
+
+export default router
