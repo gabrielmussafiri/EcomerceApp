04b3e8a4e16d2d27ee84dd8373c2bf36d7c14c22
gabriel <musgabriel2@gmail.com>
2023-08-25 00:21:53 +0200

    create header component

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<script setup>
+import {RouterLink} from "vue-router"
+</script>
+<template>
+    <div>
+        <nav class="navbar">
+            <div class="Logo">
+                <h1>MY Store</h1>
+            </div>
+            <div class="menu">
+                <RouterLink to="">Product</RouterLink>
+                <RouterLink to="">About</RouterLink>
+                <RouterLink to="">Contact</RouterLink>
+                <RouterLink to="">Card</RouterLink>
+                <p>hi</p>
+                
+            </div>
+        </nav>
+        
+    </div>
+</template>
