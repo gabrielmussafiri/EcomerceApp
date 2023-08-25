120eb801a16a1f52cbed0627e24d73e205199b47
gabriel <musgabriel2@gmail.com>
2023-08-26 01:50:50 +0200

    calculate the total of prices

--- src/views/CartPage.vue
+++ src/views/CartPage.vue
@@ -4,6 +4,11 @@ import { computed } from 'vue';
 
 const store = useStore();
 const cartItems = computed(() => store.getters.cartItems);
+// Calculate the total price
+const total = computed(() => {
+  return cartItems.value.reduce((total, product) => total + product.price, 0);
+});
+
 </script>
 
 <template>
@@ -11,8 +16,18 @@ const cartItems = computed(() => store.getters.cartItems);
       <div class="cart">
         <h2>Your Cart</h2>
         <ul>
-          <li v-for="(cartProduct, index) in cartItems" :key="index">{{ cartProduct.title }}</li>
+          <li v-for="(cartProduct, index) in cartItems" :key="index">
+            <div class="card">
+              <div class="img"><img :src="cartProduct.image" alt=""></div>
+              <div class="title">{{ cartProduct.title }}</div>
+              <div class="price">${{ cartProduct.price }}</div>
+            </div>
+          </li>
         </ul>
+        <div class="total">Total: ${{ total }}</div>
       </div>
     </main>
   </template>
+  <style scoped>
+</style>
+  
