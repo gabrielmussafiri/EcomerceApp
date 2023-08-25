bc8a385401149c13b553fed2fa1dadd0fe7e0e11
gabriel <musgabriel2@gmail.com>
2023-08-25 10:19:33 +0200

    logo style

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,9 @@
 
 import { createRouter,createWebHistory } from "vue-router";
 import ProductView from '../views/ProductView.vue';
-import AboutView from '../views/AboutView.vue';
+import AboutView from "../views/AboutView.vue";
+import ContactView from '../views/ContactView.vue';
+
 
 
 
@@ -24,7 +26,13 @@ const router = createRouter({
         {
             path:"/about",
             name:"about",
-            component:AboutView,
+            component:AboutView
+            
+        },
+        {
+            path:"/contacts",
+            name:"contacts",
+            component:ContactView
             
         }
     ]
