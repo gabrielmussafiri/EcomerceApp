98f2f5390a88ea851cc4bb45dee7830c575ef791
gabriel <musgabriel2@gmail.com>
2023-08-25 10:40:16 +0200

    fetchData from API

---
+++ src/views/ItemsView.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+    <h1>Here is all Products</h1>
+        
+    </div>
+</template>
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,6 +1,34 @@
+<script setup>
+import ItemsView from '../views/ItemsView.vue';
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const products = ref([]);
+const cart = ref([]);
+
+const fetchProducts = () => {
+  axios.get('https://fakestoreapi.com/products')
+    .then(response => {
+      products.value = response.data;
+    })
+    .catch(error => {
+      console.error('Error fetching products:', error);
+    });
+};
+
+const addToCart = (product) => {
+  cart.value.push(product);
+};
+
+onMounted(fetchProducts);
+
+</script>
+
 <template>
     <main>
         <h1>Product</h1>
+        <ItemsView/>
+        
         
     </main>
 </template>
